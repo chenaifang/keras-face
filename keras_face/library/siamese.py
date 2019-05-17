dfc3175b8dccfc22e37098f8f5c534f424f358dd
IT <34065949+chenaifang@users.noreply.github.com>
2019-05-17 11:20:16 +0800

    Update siamese.py

--- keras_face/library/siamese.py
+++ keras_face/library/siamese.py
@@ -443,17 +443,17 @@ def main():
     model_dir_path = './model'
     #image_dir_path = "./data/dlib-align-images"
     #在测试
-    image_dir_path = "./dlib-align-images7"
+    image_dir_path = "./dlib-align-images2"
     database = dict()
     
     
-    for filename in os.listdir(r"./dlib-align-images7"):
+    for filename in os.listdir(r"./dlib-align-images2"):
         pic_list=[]
-        for picname in os.listdir(r"./dlib-align-images7/"+filename):
+        for picname in os.listdir(r"./dlib-align-images2/"+filename):
             pic_list.append(fnet.img_to_encoding(image_dir_path+"/"+filename+"/"+picname))
         database[filename]=pic_list
     
-    np.save('picture_encoding7.npy', database)
+    np.save('picture_encoding2.npy', database)
     
     #read_dictionary = np.load("picture_encoding2.npy").item()
     
