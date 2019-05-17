8681cf49a36943d4f178d4e19fd2cc3c34e4f272
IT <34065949+chenaifang@users.noreply.github.com>
2019-05-17 16:07:33 +0800

    Update siamese.py

--- keras_face/library/siamese.py
+++ keras_face/library/siamese.py
@@ -327,7 +327,7 @@ class SiameseFaceNet(object):
         hist=self.model.fit([t_x[:, 0], t_x[:, 1]], t_y,
                        batch_size=batch_size,
                        epochs=epochs,
-                       validation_split=0.4,
+                       validation_split=0.3,
                        verbose=SiameseFaceNet.VERBOSE,
                        callbacks=[checkpoint],
                        shuffle=True)
