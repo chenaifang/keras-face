624443706ec8e04ba7a33c1f1b31fffc47500404
IT <34065949+chenaifang@users.noreply.github.com>
2019-05-19 19:23:17 +0800

    Update siamese.py

--- keras_face/library/siamese.py
+++ keras_face/library/siamese.py
@@ -60,7 +60,7 @@ class SiameseFaceNet(object):
         self.labels = None
         self.config = None
         self.input_shape = (224,224,3)
-        self.threshold = 0.5
+        self.threshold = 0.7
         self.vgg16_model = None
 
     #完成图像的解码，大小处理，预处理，生成模型的输入
@@ -116,14 +116,14 @@ class SiameseFaceNet(object):
         '''
         x = Flatten()(input) #Tensor("flatten_1_1/Reshape:0", shape=(?, ?), dtype=float32)
         #Dense（全连接层）的两个参数：units：大于0的整数，代表该层的输出维度。activation：激活函数，为预定义的激活函数名（参考激活函数），或逐元素（element-wise）的Theano函数。如果不指定该参数，将不会使用任何激活函数（即使用线性激活函数：a(x)=x）
-        x = Dense(2048, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x) #Tensor("dense_1/Relu:0", shape=(?, 128), dtype=float32)
+        x = Dense(1024, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x) #Tensor("dense_1/Relu:0", shape=(?, 128), dtype=float32)
         #dropout概率，输出的非0元素是原来的 “1/keep_prob” 倍
         x = Dropout(0.5)(x) #Tensor("dropout_1/cond/Merge:0", shape=(?, 128), dtype=float32)
         x = Dense(2048, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x) #Tensor("dense_2/Relu:0", shape=(?, 128), dtype=float32)
         x = Dropout(0.5)(x) #Tensor("dropout_2/cond/Merge:0", shape=(?, 128), dtype=float32)
-        x = Dense(2048, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x)
-        x = Dropout(0.5)(x)
-        x = Dense(128, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x) #Tensor("dense_3/Relu:0", shape=(?, 128), dtype=float32)
+        #x = Dense(2048, activation='sigmoid',kernel_initializer='random_uniform',bias_initializer='zeros')(x)
+        #x = Dropout(0.5)(x)
+        x = Dense(256, activation='softmax',kernel_initializer='random_uniform',bias_initializer='zeros')(x) #Tensor("dense_3/Relu:0", shape=(?, 128), dtype=float32)
         #input:Tensor("input_3:0", shape=(?, 1, 1000), dtype=float32);x:Tensor("dense_3/Relu:0", shape=(?, 128), dtype=float32)
         model=Model(input,x)
         plot_model(model, to_file='base_model.png', show_shapes=True)
